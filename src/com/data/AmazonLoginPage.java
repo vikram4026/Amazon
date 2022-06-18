d53b70e6af52a2d5914f6e7f5c781eac815b2ed0
vikram4026 <gmvikramgmv@gmail.com>
2022-06-18 16:39:08 +0530

    Vino work has finished

--- src/com/data/AmazonLoginPage.java
+++ src/com/data/AmazonLoginPage.java
@@ -1,12 +1,17 @@
 package com.data;
 
 public class AmazonLoginPage {
-	
-	
+
+	private void vinoWork() {
+		
+		System.out.println("Login button work finished");
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("User is  Amazon login page");
-		
+
 		System.out.println("I am a Master");
 	}
 
