da4acb0391da86ca458929590925b5e4d80bf40e
vikram4026 <gmvikramgmv@gmail.com>
2022-06-18 16:25:31 +0530

    Sam work has finnished

--- src/com/data/AmazonLoginPage.java
+++ src/com/data/AmazonLoginPage.java
@@ -2,6 +2,11 @@ package com.data;
 
 public class AmazonLoginPage {
 	
+	private void samWork() {
+
+		System.out.println("Paswword work finish");
+	}
+	
 	
 	public static void main(String[] args) {
 		
