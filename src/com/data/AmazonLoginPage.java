ca369b900b9ad701267546b302a7a8907ad95f84
vikram4026 <gmvikramgmv@gmail.com>
2022-06-18 16:59:38 +0530

    Thus

--- src/com/data/AmazonLoginPage.java
+++ src/com/data/AmazonLoginPage.java
@@ -2,19 +2,17 @@ package com.data;
 
 public class AmazonLoginPage {
 
-
 	private void vinoWork() {
-		
+
 		System.out.println("Login button work finished");
 
 	}
 
-
-	
 	private void samWork() {
 
 		System.out.println("Paswword work finish");
 	}
+
 	private void arunWork() {
 
 		System.out.println("Email ");
@@ -22,8 +20,11 @@ public class AmazonLoginPage {
 		System.out.println(2);
 		System.out.println(3);
 	}
-	
-	
+
+	private void clean() {
+		System.out.println("Done");
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("User is  Amazon login page");
