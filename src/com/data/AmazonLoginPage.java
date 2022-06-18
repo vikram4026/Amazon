850ffd17be2a242c0ec218265d6b15226bb90b28
vikram4026 <gmvikramgmv@gmail.com>
2022-06-18 15:23:00 +0530

    Login Functionality

---
+++ src/com/data/AmazonLoginPage.java
@@ -0,0 +1,13 @@
+package com.data;
+
+public class AmazonLoginPage {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("User is  Amazon login page");
+		
+		System.out.println("I am a Master");
+	}
+
+}
